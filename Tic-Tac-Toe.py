aa68865295c32083e6e0aa6e823edb8bf0a1226c
Tari5-code <135033273+Tari5-code@users.noreply.github.com>
2023-08-02 16:37:10 +0200

    Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,20 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ___
-# 
-# <a href='https://www.udemy.com/user/joseportilla/'><img src='../Pierian_Data_Logo.png'/></a>
-# ___
-# <center><em>Content Copyright by Pierian Data</em></center>
-
-# # Milestone Project 1: Full Walk-through Code Solution
-# 
-# Below is the filled in code that goes along with the complete walk-through video. Check out the corresponding lecture videos for more information on this code!
 
 # **Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation.**
-
-# In[1]:
-
 
 from IPython.display import clear_output
 
@@ -36,16 +21,11 @@ def display_board(board):
 
 # **TEST Step 1:** run your function on a test version of the board list, and make adjustments as necessary
 
-# In[2]:
-
-
 test_board = ['#','X','O','X','O','X','O','X','O','X']
 display_board(test_board)
 
 
 # **Step 2: Write a function that can take in a player input and assign their marker as 'X' or 'O'. Think about using *while* loops to continually ask until you get a correct answer.**
-
-# In[3]:
 
 
 def player_input():
@@ -62,16 +42,11 @@ def player_input():
 
 # **TEST Step 2:** run the function to make sure it returns the desired output
 
-# In[4]:
-
 
 player_input()
 
 
 # **Step 3: Write a function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board.**
-
-# In[10]:
-
 
 def place_marker(board, marker, position):
     board[position] = marker
@@ -79,16 +54,12 @@ def place_marker(board, marker, position):
 
 # **TEST Step 3:** run the place marker function using test parameters and display the modified board
 
-# In[11]:
-
 
 place_marker(test_board,'$',8)
 display_board(test_board)
 
 
 # **Step 4: Write a function that takes in a board and checks to see if someone has won. **
-
-# In[12]:
 
 
 def win_check(board,mark):
@@ -105,15 +76,11 @@ def win_check(board,mark):
 
 # **TEST Step 4:** run the win_check function against our test_board - it should return True
 
-# In[13]:
-
 
 win_check(test_board,'X')
 
 
 # **Step 5: Write a function that uses the random module to randomly decide which player goes first. You may want to lookup random.randint() Return a string of which player went first.**
-
-# In[14]:
 
 
 import random
@@ -127,8 +94,6 @@ def choose_first():
 
 # **Step 6: Write a function that returns a boolean indicating whether a space on the board is freely available.**
 
-# In[15]:
-
 
 def space_check(board, position):
     
@@ -136,8 +101,6 @@ def space_check(board, position):
 
 
 # **Step 7: Write a function that checks if the board is full and returns a boolean value. True if full, False otherwise.**
-
-# In[16]:
 
 
 def full_board_check(board):
@@ -148,8 +111,6 @@ def full_board_check(board):
 
 
 # **Step 8: Write a function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if its a free position. If it is, then return the position for later use. **
-
-# In[17]:
 
 
 def player_choice(board):
@@ -163,8 +124,6 @@ def player_choice(board):
 
 # **Step 9: Write a function that asks the player if they want to play again and returns a boolean True if they do want to play again.**
 
-# In[18]:
-
 
 def replay():
     
@@ -172,9 +131,6 @@ def replay():
 
 
 # **Step 10: Here comes the hard part! Use while loops and the functions you've made to run the game!**
-
-# In[ ]:
-
 
 print('Welcome to Tic Tac Toe!')
 
@@ -235,9 +191,6 @@ while True:
         break
 
 
-# ## Good Job!
-
-# In[ ]:
 
 
 
